5a6e071ea8c6fdd14a17a361a30327ef91b6ef63
sathvik200 <karanthsathvik@gmail.com>
2025-12-29 11:03:51 +0530

    updated task #1

--- Python Programming/Assignment #2/Task1.py
+++ Python Programming/Assignment #2/Task1.py
@@ -1,5 +1,2 @@
 num = int(input("Enter your number: "))
-if num % 2 == 0:
-    print(f"{num} is an even number.")
-else:
-    print(f"{num} is an odd number.")
+print(f"{num} is an even number.") if num%2 == 0 else print(f"{num} is an odd number.")
